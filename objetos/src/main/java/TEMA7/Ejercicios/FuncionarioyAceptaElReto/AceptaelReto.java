0002ee8a1cdc393367fa5a240a56d391d4ff9781
DESKTOP-20CT5AN\joaqu <joaquinmagnoli10@gmail.com>
2026-03-10 10:55:08 +0100

    sdsds

--- objetos/src/main/java/TEMA7/Ejercicios/FuncionarioyAceptaElReto/AceptaelReto.java
+++ objetos/src/main/java/TEMA7/Ejercicios/FuncionarioyAceptaElReto/AceptaelReto.java
@@ -1,12 +1,11 @@
 package TEMA7.Ejercicios.FuncionarioyAceptaElReto;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.TreeSet;
 
 public class AceptaelReto {
 
-            static Scanner read = new Scanner(System.in);
+            static Scanner teclado = new Scanner(System.in);
 
             public static void main(String[] args) {
                 while (casoDePrueba()) {
@@ -15,14 +14,42 @@ public class AceptaelReto {
 
             public static boolean casoDePrueba() {
                 //leer caso de prueba
-                int y = read.nextInt();
-                read.nextLine();
+                int y = teclado.nextInt();
+                teclado.nextLine();
 
                 if (y == 0)
                     return false;
                 else {
                     // CÓDIGO PRINCIPAL AQUÍ
-                    Set<String> potito = new HashSet<>();
+                    TreeSet<String> potitoBueno = new TreeSet<>();
+                    TreeSet<String> potitoMalo = new TreeSet<>();
+
+                    for (int i = 0; i < y; i++) {
+                        String frase = teclado.nextLine();
+                        String[] palabras = frase.split(" ");
+                        for (int j = 0; j < palabras.length-1; j++) {
+                            if (palabras[0].equals("SI:") && j == 0)continue;
+                            if (palabras[0].equals("SI:")){
+                                potitoBueno.add(palabras[j]);
+                            }
+                            if (palabras[0].equals("NO:") && j == 0)continue;
+                            if (palabras[0].equals("NO:")) {
+                                potitoMalo.add(palabras[j]);
+                            }
+                        }
+                    }
+
+                    boolean espacio = false;
+                    for (String potito : potitoMalo){
+                        if (!potitoBueno.contains(potito)){
+                            if (espacio){
+                                System.out.print(" ");
+                            }
+                            System.out.print(potito);
+                            espacio = true;
+                        }
+                    }
+                    System.out.println();
                     return true;
                 }
             }
