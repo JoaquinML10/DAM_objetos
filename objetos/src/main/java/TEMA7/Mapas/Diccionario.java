d3ca5a47fbc512b72de02adf42ddaa33e1cfd5f6
Joaquin <joaquinmagnoli10@gmail.com>
2026-03-11 21:18:57 +0100

    empezanod

--- objetos/src/main/java/TEMA7/Mapas/Diccionario.java
+++ objetos/src/main/java/TEMA7/Mapas/Diccionario.java
@@ -1,4 +1,131 @@
 package TEMA7.Mapas;
 
+import java.util.HashMap;
+
 public class Diccionario {
+    static HashMap<String,String> mapa = new HashMap<>();
+    static void main(String[] args) {
+            llamarApar();
+        System.out.println("Bienvenido al juego del diccionario SIUUUUUUUUUUUUUUUUUU");
+        while (true){
+                 
+        }
+    }
+    static public void nuevoPar(String esp, String inglish){
+        mapa.put(esp,inglish);
+    }
+    static public void traduce(){
+
+    }
+    static public void palabraAleatoria(){
+
+    }
+    static public void primeraLetraTraduccion(){
+
+    }
+    static public void llamarApar(){
+        nuevoPar("casa", "house");
+        nuevoPar("perro", "dog");
+        nuevoPar("gato", "cat");
+        nuevoPar("mesa", "table");
+        nuevoPar("silla", "chair");
+        nuevoPar("puerta", "door");
+        nuevoPar("ventana", "window");
+        nuevoPar("coche", "car");
+        nuevoPar("calle", "street");
+        nuevoPar("ciudad", "city");
+        nuevoPar("pueblo", "town");
+        nuevoPar("escuela", "school");
+        nuevoPar("libro", "book");
+        nuevoPar("cuaderno", "notebook");
+        nuevoPar("boligrafo", "pen");
+        nuevoPar("lapiz", "pencil");
+        nuevoPar("papel", "paper");
+        nuevoPar("mochila", "backpack");
+        nuevoPar("profesor", "teacher");
+        nuevoPar("estudiante", "student");
+        nuevoPar("amigo", "friend");
+        nuevoPar("familia", "family");
+        nuevoPar("madre", "mother");
+        nuevoPar("padre", "father");
+        nuevoPar("hermano", "brother");
+        nuevoPar("hermana", "sister");
+        nuevoPar("niño", "child");
+        nuevoPar("niña", "girl");
+        nuevoPar("hombre", "man");
+        nuevoPar("mujer", "woman");
+        nuevoPar("comida", "food");
+        nuevoPar("agua", "water");
+        nuevoPar("leche", "milk");
+        nuevoPar("pan", "bread");
+        nuevoPar("queso", "cheese");
+        nuevoPar("carne", "meat");
+        nuevoPar("pollo", "chicken");
+        nuevoPar("pescado", "fish");
+        nuevoPar("fruta", "fruit");
+        nuevoPar("verdura", "vegetable");
+        nuevoPar("manzana", "apple");
+        nuevoPar("naranja", "orange");
+        nuevoPar("platano", "banana");
+        nuevoPar("uva", "grape");
+        nuevoPar("fresa", "strawberry");
+        nuevoPar("azucar", "sugar");
+        nuevoPar("sal", "salt");
+        nuevoPar("cafe", "coffee");
+        nuevoPar("te", "tea");
+        nuevoPar("desayuno", "breakfast");
+        nuevoPar("almuerzo", "lunch");
+        nuevoPar("cena", "dinner");
+        nuevoPar("trabajo", "work");
+        nuevoPar("oficina", "office");
+        nuevoPar("empresa", "company");
+        nuevoPar("dinero", "money");
+        nuevoPar("tienda", "shop");
+        nuevoPar("mercado", "market");
+        nuevoPar("hospital", "hospital");
+        nuevoPar("farmacia", "pharmacy");
+        nuevoPar("parque", "park");
+        nuevoPar("playa", "beach");
+        nuevoPar("montaña", "mountain");
+        nuevoPar("rio", "river");
+        nuevoPar("mar", "sea");
+        nuevoPar("cielo", "sky");
+        nuevoPar("sol", "sun");
+        nuevoPar("luna", "moon");
+        nuevoPar("estrella", "star");
+        nuevoPar("dia", "day");
+        nuevoPar("noche", "night");
+        nuevoPar("mañana", "morning");
+        nuevoPar("tarde", "afternoon");
+        nuevoPar("tiempo", "time");
+        nuevoPar("hora", "hour");
+        nuevoPar("minuto", "minute");
+        nuevoPar("segundo", "second");
+        nuevoPar("año", "year");
+        nuevoPar("semana", "week");
+        nuevoPar("mes", "month");
+        nuevoPar("invierno", "winter");
+        nuevoPar("verano", "summer");
+        nuevoPar("primavera", "spring");
+        nuevoPar("otoño", "autumn");
+        nuevoPar("frio", "cold");
+        nuevoPar("calor", "heat");
+        nuevoPar("lluvia", "rain");
+        nuevoPar("viento", "wind");
+        nuevoPar("nieve", "snow");
+        nuevoPar("rapido", "fast");
+        nuevoPar("lento", "slow");
+        nuevoPar("grande", "big");
+        nuevoPar("pequeño", "small");
+        nuevoPar("alto", "tall");
+        nuevoPar("bajo", "short");
+        nuevoPar("nuevo", "new");
+        nuevoPar("viejo", "old");
+        nuevoPar("bueno", "good");
+        nuevoPar("malo", "bad");
+        nuevoPar("feliz", "happy");
+        nuevoPar("triste", "sad");
+        nuevoPar("facil", "easy");
+        nuevoPar("dificil", "difficult");
+    }
 }
